c7df435f2247dc1dc307017537b483cd864de16c
Mateus Reis <you@example.com>
2022-08-29 17:35:23 -0300

    useDrop, contextAPI & Install immerjs

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,18 +1,57 @@
-import React from 'react'
+import React, { useRef, useContext } from 'react'
+
+import BoardContext from '../Board/context';
+
 import { Container, Label } from '../Card/styles'
 
-import { useDrag } from 'react-dnd'
+import { useDrag, useDrop } from 'react-dnd'
 
-export default function Card({ data }) {
+export default function Card({ data, index }) {
+  const ref = useRef();
+  const { move } = useContext(BoardContext);
+
   const [{ isDragging }, dragRef] = useDrag({
     type: 'CARD',
+    item: {
+      index,
+    },
     collect: monitor => ({
       isDragging: monitor.isDragging(),
     })
   })
+
+  const [, dropRef] = useDrop({
+    accept: 'CARD',
+    hover(item, monitor) {
+      const draggedIndex = item.index
+      const targetIndex = index
+
+      if(draggedIndex === targetIndex) {
+        return;
+      }
+
+      const targetSize = ref.current.getBoundingClientRect();
+      const targetCenter = (targetSize.bottom - targetSize.top) / 2;
+
+      const draggedOffset = monitor.getClientOffset();
+      const draggedTop = draggedOffset.y - targetSize.top;
+
+      if(draggedIndex < targetIndex && draggedTop < targetCenter) {
+        return;
+      }
+
+      if(draggedIndex > targetIndex && draggedTop > targetCenter) {
+        return;
+      }
+
+      move(draggedIndex, targetIndex)
+    }
+  })
+
+  dragRef(dropRef(ref))
   
   return (
-    <Container ref={dragRef} isDragging={isDragging}>
+    <Container ref={ref} isDragging={isDragging}>
       <header>
         {data.labels.map(label => <Label key={label} color={label} />)}
       </header>
