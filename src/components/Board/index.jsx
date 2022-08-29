438c459dabfd31441cfa8315e0e3f71aa20776c5
Mateus Reis <you@example.com>
2022-08-29 17:58:27 -0300

    finalizando tutorial

--- src/components/Board/index.jsx
+++ src/components/Board/index.jsx
@@ -6,20 +6,26 @@ import List from '../List'
 import { loadLists } from '../../services/api'
 
 import { Container } from './styles'
+import produce from 'immer'
 
 const data = loadLists();
 
 export default function Board() {
   const [lists, setLists] = useState(data);
 
-  function move(from, to) {
-    console.log(from, to)
+  function move(fromList, toList, from, to) {
+    setLists(produce(lists, draft => {
+      const dragged = draft[fromList].cards[from];
+      
+      draft[fromList].cards.splice(from, 1);
+      draft[toList].cards.splice(to, 0, dragged);
+    }))
   }
   
   return (
     <BoardContext.Provider value={{ lists, move }}>
       <Container>
-        {lists.map(list => <List key={list.title} data={list} />)}
+        {lists.map((list, index) => <List key={list.title} index={index} data={list} />)}
       </Container>
     </BoardContext.Provider>
   )
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -6,7 +6,7 @@ import { Container, Label } from '../Card/styles'
 
 import { useDrag, useDrop } from 'react-dnd'
 
-export default function Card({ data, index }) {
+export default function Card({ data, index, listIndex }) {
   const ref = useRef();
   const { move } = useContext(BoardContext);
 
@@ -14,6 +14,7 @@ export default function Card({ data, index }) {
     type: 'CARD',
     item: {
       index,
+      listIndex,
     },
     collect: monitor => ({
       isDragging: monitor.isDragging(),
@@ -23,10 +24,13 @@ export default function Card({ data, index }) {
   const [, dropRef] = useDrop({
     accept: 'CARD',
     hover(item, monitor) {
+      const draggedListIndex = item.listIndex;
+      const targetListIndex = listIndex;
+
       const draggedIndex = item.index
       const targetIndex = index
 
-      if(draggedIndex === targetIndex) {
+      if(draggedIndex === targetIndex && draggedListIndex === targetListIndex) {
         return;
       }
 
@@ -44,7 +48,10 @@ export default function Card({ data, index }) {
         return;
       }
 
-      move(draggedIndex, targetIndex)
+      move(draggedListIndex, targetListIndex, draggedIndex, targetIndex);
+
+      item.index = targetIndex;
+      item.listIndex = targetListIndex;
     }
   })
 
