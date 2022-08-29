d10573b90a4b85582c0aa558fdcc67b8f3fb2480
Mateus Reis <you@example.com>
2022-08-29 18:26:41 -0300

    ajuste import google fonts

--- src/styles/globals.js
+++ src/styles/globals.js
@@ -1,8 +1,6 @@
 import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Roboto:400,500&display=swap');
-
   * {
     margin: 0;
     padding: 0;
@@ -15,7 +13,7 @@ export default createGlobalStyle`
   }
 
   body {
-    font: 14px 'Roboto', sans-serif;
+    font: 14px 'Poppins', sans-serif;
     background: #ecf1f8;
     -webkit-font-smoothing: antialiased !important;
   }
