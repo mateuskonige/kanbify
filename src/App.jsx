187d40108e991b74f6809297413aaacd2aa3fed2
Mateus Reis <you@example.com>
2022-08-29 16:14:12 -0300

    react-dnd && isDragging

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,17 @@ import Header from "./components/Header"
 
 import GlobalStyle from './styles/globals'
 
+import { HTML5Backend } from 'react-dnd-html5-backend'
+import { DndProvider } from 'react-dnd'
+
 function App() {
   return (
-    <>
+    <DndProvider backend={HTML5Backend}>
       <Header />
       <Board />
       
       <GlobalStyle />
-    </>
+    </DndProvider>
   )
 }
 
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import { Container, Label } from '../Card/styles'
 
+import { useDrag } from 'react-dnd'
+
 export default function Card({ data }) {
+  const [{ isDragging }, dragRef] = useDrag({
+    type: 'CARD',
+    collect: monitor => ({
+      isDragging: monitor.isDragging(),
+    })
+  })
+  
   return (
-    <Container>
+    <Container ref={dragRef} isDragging={isDragging}>
       <header>
         {data.labels.map(label => <Label key={label} color={label} />)}
       </header>
