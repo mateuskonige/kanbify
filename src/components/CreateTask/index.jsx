3c43ee82843b336deee5ea0d030b23f4ce6fc83f
Mateus Reis <you@example.com>
2022-08-30 16:58:14 -0300

    implementando modal de criação de tarefa

---
+++ src/components/CreateTask/index.jsx
@@ -0,0 +1,10 @@
+import { Container } from './styles';
+
+export function CreateTask() {
+  return (
+    <Container>
+      <button>close</button>
+      <h1>CreateTask</h1>
+    </Container>
+  );
+}
--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,22 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import { MdAdd } from 'react-icons/md'
+import { MdAdd, MdClose } from 'react-icons/md'
 import Card from '../Card'
 
-import { Container } from './styles'
+import { Container, ModalContainer } from './styles'
 
 export default function List({ data, index: listIndex }) {
+  const [createTask, setCreateTask] = useState(false)
+  
+  const handleModal = () => {
+    if(createTask == false) {
+      console.log(createTask)
+      setCreateTask(true)
+    } else {
+      console.log(createTask)
+      setCreateTask(false)
+    }
+  }
+
   return (
     <Container done={data.done}>
       <header>
         <h2>{data.title}</h2>
         {data.creatable && (
-          <button type='button'>
+          <button type='button' onClick={handleModal}>
             <MdAdd size={24} color="#fff" />
           </button>
         )}
       </header>
+     
+{createTask && (
+  <ModalContainer>
+    <section>
+      <header>
+      <h2>Adicionar tarefa</h2>
+      <button onClick={handleModal}><MdClose size={24} color="#fff" /></button>
+      </header>
+      <main>
+        <div>
+          <label htmlFor="">Título</label>
+          <input type="text" />
 
+          <label htmlFor="">Cor</label>
+          <input type="color" />
+        </div>
+      </main>
+    </section>
+  </ModalContainer>
+)}
+     
       <ul>
         {data.cards.map((card, index) => (
           <Card
